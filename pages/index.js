797479d923e3966bece162bcc24ce874bfef6a0a
Andy <bungandy@gmail.com>
2023-11-14 19:28:58 +0700

    wip: header

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Logo from '@/components/Logo'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Logo from "@/components/Logo";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className='bg-primary-200 w-screen h-screen grid place-items-center text-5xl'>
+    <main className="w-screen h-screen grid place-items-center text-5xl">
       <Logo />
     </main>
-  )
+  );
 }
