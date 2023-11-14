8897fba06d273b993fd14c4a6cb43c50d8c0470b
Andy <bungandy@gmail.com>
2023-11-14 19:14:34 +0700

    style: change landing page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Logo from '@/components/Logo'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='w-screen h-screen grid place-items-center text-5xl'>Re.Fresh :-)</main>
+    <main className='bg-primary-200 w-screen h-screen grid place-items-center text-5xl'>
+      <Logo />
+    </main>
   )
 }
