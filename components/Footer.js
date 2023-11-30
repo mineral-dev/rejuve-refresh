dd4aa7a331682e2fdf36c1cac3e5bbce52215283
Andy <bungandy@gmail.com>
2023-11-30 12:20:37 +0700

    chore: add footer link

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import LogoRejuve from "./LogoRejuve";
 
 export default function Footer() {
   return (
     <footer className="bg-primary-200 text-primary-600 grid place-items-center py-4 lg:py-12">
-      <span className="[&>svg]:h-4 lg:[&>svg]:h-auto">
-        <LogoRejuve />
-      </span>
+      <Link href="https://www.rejuve.co.id" target="_blank" rel="noopener noreferrer">
+        <span className="[&>svg]:h-4 lg:[&>svg]:h-auto">
+          <LogoRejuve />
+        </span>
+      </Link>
     </footer>
   );
 }
