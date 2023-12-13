30abbb2db77f8019f0430ffa9be421a67f2035f8
Andy <bungandy@gmail.com>
2023-12-13 11:40:22 +0700

    typo: url footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import LogoRejuve from "./LogoRejuve";
 export default function Footer() {
   return (
     <footer className="bg-primary-200 text-primary-600 grid place-items-center py-4 lg:py-12 relative">
-      <Link href="https://www.rejuce.co.id" target="_blank" rel="noopener noreferrer" className="[&>svg]:h-4 lg:[&>svg]:h-auto">
+      <Link href="https://www.rejuve.co.id" target="_blank" rel="noopener noreferrer" className="[&>svg]:h-4 lg:[&>svg]:h-auto">
         <LogoRejuve />
       </Link>
       <div className="absolute right-0 pr-4">
